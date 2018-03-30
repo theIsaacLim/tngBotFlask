708aeeddbf19ca7de11f02b14f74b846b62680d9
Isaac Lim <r1522660@mvrht.net>
2018-03-30 12:57:42 +0800

    Modified requirements.txt to an actual workign state

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import markovify
 from flask import Flask
 
-nModel = markovify.Text.from_json(open("markov.json").read()) # reading a trained model from a JSON file and using that instead
+nModel = markovify.Text.from_json(open("markov.json").read())  # reading a trained model from a JSON file and using that instead
 
 def generateSentence():
     global nModel
     str = ""
-    for i in range(20): #Generates 20 sentences
+    for i in range(20):  # Generates 20 sentences
         try:
-            str += nModel.make_short_sentence(500, max_retries=200).replace("\\n", "\n") # S
+            str += nModel.make_short_sentence(500, max_retries=200).replace("\\n", "\n")
         except:
             pass
     return str
